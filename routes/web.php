09c0e2f504f428b291d34bbb3cb690f81964bef3
andre <feliciusandreas01@gmail.com>
2024-10-03 17:34:02 +0800

    pengarang

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,11 @@ use App\Http\Controllers\KategoriController;
 
 Route::get('/kategori', [kategoriController::class,'loadAllCategory']);
 Route::get('/kategori/add-category', [kategoriController::class,'loadAddCategory']);
+use App\Http\Controllers\PenerbitController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/penerbit', [PenerbitController::class, 'index'])->name('penerbit.index');
+Route::get('/penerbit/create', [PenerbitController::class, 'create'])->name('penerbit.create');
+Route::post('/penerbit/store', [])->name('penerbit.store');
